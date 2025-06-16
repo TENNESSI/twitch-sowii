f7d2cbe8a81554cf928c50ce8c8ecedbf46f8e57
TENNESSI <bratuhin-kostya99@mail.ru>
2025-06-16 16:56:36 +0400

    отслеживание начала матча, нужен тест, не проходит проверку для выполнения второго запроса
    Signed-off-by: TENNESSI <bratuhin-kostya99@mail.ru>

---
+++ start_tracker.py
@@ -0,0 +1,74 @@
+import os
+import asyncio
+import aiohttp
+from anyio import current_time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+STEAM_API_KEY = os.getenv('STEAM_API_KEY')
+OPEN_DOTA_API = 'https://api.opendota.com/api'
+
+class MatchTracker:
+    def __init__(self):
+        self.tracked_players = {}
+        self.current_matches = {}
+        self.session = aiohttp.ClientSession()
+
+    async def add_player(self, steam_id_64):
+        self.tracked_players[steam_id_64] = {
+            'last_status': None,
+            'current_match': None
+        }
+        print(f'Начато отслеживание игрока {steam_id_64}')
+
+    async def check_player_status(self, steam_id_64):
+        try:
+            url = f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={STEAM_API_KEY}&steamids={steam_id_64}'
+            async with self.session.get(url) as response:
+                print(url)
+                print(response.status)
+                data = await response.json(content_type=None)
+                print(data)
+                player = data['response']['players'][0]
+
+                if 'gameid' in player and player['gameid'] == '570':
+                    opendota_url = f'{OPEN_DOTA_API}/players/{steam_id_64[3:]}/recrentMatches'
+                    async with self.session.get(opendota_url) as opendota_response:
+                        print(f'Запрос {opendota_url} статус {response.status}')
+                        matches = await opendota_response.json(content_type=None)
+                        current_match = next((m for m in matches if m['lobby_type'] == 7), None)
+
+                        if current_match:
+                            return {
+                                'in_game': True,
+                                'match_id': current_match['match_id'],
+                                'start_time': current_match['start_time']
+                            }
+            return {'in_game': False}
+        except Exception as _e:
+            print(f'Ошибка при проверке статуса: {_e}')
+            return {'in_game': False}
+
+    async def start_monitoring(self, interval = 60):
+        while True:
+            for steam_id in list(self.tracked_players.keys()):
+                status = await self.check_player_status(steam_id)
+                prev_status = self.tracked_players[steam_id]['last_status']
+
+                if status['in_game'] != (prev_status['in_game'] if prev_status else False):
+                    if status['in_game']:
+                        print(f'Игрок {steam_id} начал матч! ID: {status["match_id"]}')
+
+                    self.tracked_players[steam_id]['last_status'] = status
+                    self.tracked_players[steam_id]['current_match'] = status.get('match_id')
+
+            await asyncio.sleep(interval)
+
+async def main():
+    tracker = MatchTracker()
+    await tracker.add_player(os.getenv('steam_id'))
+    await tracker.start_monitoring()
+
+if __name__ == '__main__':
+    asyncio.run(main())
